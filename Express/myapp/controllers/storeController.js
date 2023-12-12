b180ced7b74647622d56bff64da4a1a7fcdc6ae9
Collin <cdchamberlin.rva@gmail.com>
2023-12-11 22:48:55 -0500

    Connnected front and back end.

--- Express/myapp/controllers/storeController.js
+++ Express/myapp/controllers/storeController.js
@@ -1,1 +1,13 @@
-const axios = require("axios")
+const axios = require("axios");
+const { response } = require("express");
+
+const getProductList = (req, res) => {
+  try {
+    axios
+      .get("https://fakestoreapi.com/products")
+      .then((response) => res.status(200).json(response.data));
+  } catch (error) {
+    res.status(500).json({ result: "External server error, " + error.message });
+  }
+};
+module.exports = { getProductList };
